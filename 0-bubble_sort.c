94a54a721307e48830c48f4eb68a4c2e56c73493
Bertin <irankundab21ertin@gmail.com>
2024-01-19 23:18:29 -0800

    3rd commit

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -19,9 +19,9 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[j] > array[j + 1])
 			{
-				temp = array[j];
+				tmp = array[j];
 				array[j] = array[j + 1];
-				array[j + 1] = temp;
+				array[j + 1] = tmp;
 				print_array(array, size);
 			}
 		}

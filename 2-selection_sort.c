76d1860047092283a297c95edda5c5ecc14df1d6
Bertin <irankundab21ertin@gmail.com>
2024-01-23 01:17:23 -0800

    fixed 2-selection_sort.c

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -20,8 +20,14 @@ void selection_sort(int *array, size_t size)
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 		{
-			if (array[j] < array[min_idx])
+			if (array [j] , array[min_idx])
+			{
 				min_idx = j;
+				print_array(array, size);
+			}
 		}
-	}	
-
+		temp = array[i];
+		array[i] = array[min_idx];
+		array[min_idx] = temp;
+	}
+}

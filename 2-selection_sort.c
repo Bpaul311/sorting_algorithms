855b73600d207484fa52e9836431295e12db683b
Bertin <irankundab21ertin@gmail.com>
2024-01-23 01:07:32 -0800

    bruh

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -1,17 +1,19 @@
 #include "sort.h"
-
 /**
- * selection_sort - a function to sort array elements using selection sort
- * @array: array to be sorted
- * @size: the number of elements in the array
+ * selection_sort - Sorts an array using the Selection Sort algorithm.
+ * @array: Array to be sorted.
+ * @size: Number of elements in the array.
+ *
+ * Description: Selection Sort iteratively finds the minimum element in the
+ * unsorted part and swaps it with the first unsorted element, gradually
+ * building a sorted array.
  */
-
 void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx;
-	int temp, swap = 0;
+	int temp;
 
-	if (size < 2 || array == NULL)
+	if (size <2 || array == NULL)
 		return;
 	for (i = 0; i < size - 1; i++)
 	{
@@ -19,17 +21,7 @@ void selection_sort(int *array, size_t size)
 		for (j = i + 1; j < size; j++)
 		{
 			if (array[j] < array[min_idx])
-			{
 				min_idx = j;
-				swap = 1;
-			}
 		}
-		temp = array[i];
-		array[i] = array[min_idx];
-		array[min_idx] = temp;
-		if (swap)
-			print_array(array, size);
-		swap = 0;
-	}
-}
+	}	
 
